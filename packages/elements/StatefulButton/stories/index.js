4a57679a39ef58c7cbc79b4ef2ca5d8b651525b2
jukai.zhang <jukai.zhang@rea-group.com>
2018-02-01 15:26:45 +0800

    add lerna

--- packages/elements/StatefulButton/stories/index.js
+++ packages/elements/StatefulButton/stories/index.js
@@ -7,7 +7,7 @@ import StatefullReactButton from '../StatefullReactButton';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
-// storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Stateful Button')} />);
 
 storiesOf('Stateful Button', module)
   .add('stateful react Button', () => <StatefullReactButton handleOnclick={action('clicked')}/>);
--- packages/elements/StatelessButton/stories/index.js
+++ packages/elements/StatelessButton/stories/index.js
@@ -2,12 +2,7 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 import StatelessReactButton from '../StatelessReactButton';
-
-import { Button, Welcome } from '@storybook/react/demo';
-
-// storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Stateless Button', module)
   .add('stateless react Button', () => <StatelessReactButton handleOnclick={action('clicked')}/>);
