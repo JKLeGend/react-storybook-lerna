eb00fe04399829c2a49640f0ed1e97ac1ac3131d
jukai.zhang <jukai.zhang@rea-group.com>
2018-02-12 12:36:43 +0800

    test

--- packages/elements/StatefulButton/src/StatefullReactButton.js
+++ packages/elements/StatefulButton/src/StatefullReactButton.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class StateFulReactButton extends Component {
   render() {
     const { handleOnclick } = this.props;
+    
     return (
       <button onClick={handleOnclick}>react stateful button</button>
     );
